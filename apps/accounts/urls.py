5e6de9ad16384b3678517ce44cd80553b6b632ac
Zdravko <zdravko.galchov@gmail.com>
2025-06-16 18:48:56 +0100

    :construction: feat: add views and urls to accounts app

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,7 +1,26 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 
 urlpatterns = [
-    path('signin/', views.sign_in_view, name="signin"),
+    path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),  # TODO: Test when User model is in DB
+    path('register/', views.register_view, name="register"),
+
+    path('password-reset/', include([
+        path('', views.password_reset_view, name="password-reset"),
+        path('done/', views.password_reset_done_view, name="password-reset-done"),
+        path('confirm/<uidb64>/<token>/', views.password_reset_confirm_view, name="password-reset-confirm"),
+        path('complete/', views.password_reset_confirm_view, name="password-reset-confirm"),
+    ])),
+
+    path('dashboard/', views.dashboard_view, name="dashboard"),
+
+    path('profile/<int:pk>/', include([
+        path('', views.profile_details_view, name="profile-details"),
+        path('edit/', include([
+            path('', views.profile_edit_view, name="profile-edit"),
+            path('change-password/', views.change_password_view, name="change-password"),
+        ])),
+    ])),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,7 +1,53 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from urllib.request import Request
+from django.contrib.auth import logout
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
 
 
-def sign_in_view(request: Request) -> HttpResponse:
-    return render(request, 'accounts/sign-in-page.html', {'hide_navbar': True})
+def login_view(request: Request) -> HttpResponse:
+    return render(request, 'accounts/login.html', {'hide_navbar': True})
+
+
+def logout_view(request: Request) -> HttpResponse:
+    logout(request)
+    return redirect('login')
+
+
+def register_view(request: Request) -> HttpResponse:
+    return render(request, 'accounts/register.html', {'hide_navbar': True})
+
+
+password_reset_view = auth_views.PasswordResetView.as_view(
+    template_name='accounts/password_reset_request.html'
+)
+
+password_reset_done_view = auth_views.PasswordResetDoneView.as_view(
+    template_name='accounts/password_reset_done.html'
+)
+
+password_reset_confirm_view = auth_views.PasswordResetConfirmView.as_view(
+    template_name='accounts/password_reset_confirm.html'
+)
+
+password_reset_complete_view = auth_views.PasswordResetCompleteView.as_view(
+    template_name='accounts/password_reset_complete.html'
+)
+
+# @login_required   # TODO: To be implemented
+def dashboard_view(request: Request) -> HttpResponse:
+    return render(request, 'accounts/dashboard.html')
+
+
+def profile_details_view(request: Request, pk: int) -> HttpResponse:
+    return render(request, 'accounts/profile_details.html')
+
+
+def profile_edit_view(request: Request, pk: int) -> HttpResponse:
+    return render(request, 'accounts/profile_edit.html')
+
+
+def change_password_view(request: Request, pk: int) -> HttpResponse:
+    return render(request, 'accounts/profile_change_password.html')

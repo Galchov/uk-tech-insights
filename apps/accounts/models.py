3c16f91986e376d0c42ea4ac9225951ba4db87b4
Zdravko <zdravko.galchov@gmail.com>
2025-06-21 14:12:19 +0100

    :hammer: feat: upgraded user model

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -6,13 +6,35 @@ from .managers import CustomUserManager
 
 
 class CustomUser(AbstractUser):
-    username = models.CharField(
-        max_length=150,
-        unique=True,
-    )
     email = models.EmailField(
         _('email_address'),
         unique=True,
+        help_text=_('Enter a valid email address. This will be used to log in.'),
+    )
+    email_verified = models.BooleanField(
+        _('email verified'),
+        default=False,
+        help_text=_('Indicates whether the user has verified their email address.'),
+    )
+    bio = models.TextField(
+        _('biography'),
+        blank=True,
+        null=True,
+        help_text=_('Optional: Write a short bio to display on your profile.'),
+    )
+    country = models.CharField(
+        _('country of residence'),
+        max_length=50,
+        blank=True,
+        null=True,
+        help_text=_('Optional: Your current country of residence.'),
+    )
+    profile_picture = models.ImageField(
+        _('profile picture'),
+        upload_to='profile_pictures/',
+        blank=True,
+        null=True,
+        help_text=_('Optional: Upload a profile image.'),
     )
 
     USERNAME_FIELD = 'email'

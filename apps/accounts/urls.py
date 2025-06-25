81ffdcb11697668d239716c3bc1e05a5b3ae1f30
Zdravko <zdravko.galchov@gmail.com>
2025-06-25 14:52:45 +0100

    :hammer: refactor: improve accounts urls

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -7,18 +7,14 @@ urlpatterns = [
     path('logout/', views.logout_view, name="logout"),
     path('register/', views.register_view, name="register"),
 
-    path('password-reset/', include([
-        path('', views.password_reset_view, name="password_reset"),
-        path('done/', views.password_reset_done_view, name="password_reset_done"),
-        path('confirm/<uidb64>/<token>/', views.password_reset_confirm_view, name="password_reset_confirm"),
-        path('complete/', views.password_reset_complete_view, name="password_reset_complete"),
-    ])),
+    path('password-reset/', views.password_reset_view, name="password_reset"),
+    path('password-reset/done/', views.password_reset_done_view, name="password_reset_done"),
+    path('password-reset/confirm/<uidb64>/<token>/', views.password_reset_confirm_view, name="password_reset_confirm"),
+    path('password-reset/complete/', views.password_reset_complete_view, name="password_reset_complete"),
 
-    path('profile/<int:pk>/', include([
-        path('', views.dashboard_view, name="dashboard"),
-        path('edit/', views.profile_edit_view, name="profile_edit"),
-        path('password-change/', views.password_change_view, name="password_change"),
-        path('password-change-done/', views.password_change_done_view, name="password_change_done"),
-        path('delete-account/', views.delete_account_view, name="delete_account"),
-    ]))
+    path('profile/<int:pk>/', views.dashboard_view, name="dashboard"),
+    path('profile/<int:pk>/edit/', views.profile_edit_view, name="profile_edit"),
+    path('profile/<int:pk>/password-change/', views.password_change_view, name="password_change"),
+    path('profile/<int:pk>/password-change-done/', views.password_change_done_view, name="password_change_done"),
+    path('profile/<int:pk>/delete-account/', views.delete_account_view, name="delete_account"),
 ]

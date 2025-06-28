503c706586d00477a597c5c64dbee116ba921cd2
Zdravko <zdravko.galchov@gmail.com>
2025-06-28 17:28:46 +0100

    :construction: feat: register profile admin as inline

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, Profile
 from .forms import CustomRegistrationForm, CustomUserChangeForm
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profiles'
+    fk_name = 'user'
 
 
 @admin.register(CustomUser)
@@ -10,6 +17,8 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomRegistrationForm
     form = CustomUserChangeForm
     model = CustomUser
+
+    inlines = [ProfileInLine]
 
     list_display = ['username', 'email', 'is_staff', 'is_active']
     list_filter = ['username', 'email', 'is_staff', 'is_active']
@@ -53,3 +62,9 @@ class CustomUserAdmin(UserAdmin):
 
     search_fields = ['email', 'username']
     ordering = ['email']
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return []
+        
+        return super().get_inline_instances(request, obj)

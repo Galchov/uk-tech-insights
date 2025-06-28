f4934d642e9250cafe09da4ede27caaf5f715682
Zdravko <zdravko.galchov@gmail.com>
2025-06-28 17:03:34 +0100

    :construction: feat: add common model - technology

--- apps/common/models.py
+++ apps/common/models.py
@@ -127,3 +127,37 @@ class Star(models.Model):
     def __str__(self):
         return f"{self.user} starred {self.content_object}"
     
+
+class Technology(models.Model):
+    name = models.CharField(
+        _('technology name'),
+        max_length=100,
+        unique=True,
+    )
+    description = models.TextField(
+        _('description'),
+        blank=True,
+    )
+    website = models.URLField(
+        _('official website'),
+        blank=True,
+        null=True,
+    )
+    logo = models.ImageField(
+        _('logo'),
+        upload_to='technology_logos/',
+        blank=True,
+        null=True,
+        height_field=_("Upload the logo of the technology."),
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = _('technology')
+        verbose_name_plural = _('technologies')
+
+    def __str__(self):
+        return self.name

f6178236cd18b7f34c7aca089443359df24b0e3c
Zdravko <zdravko.galchov@gmail.com>
2025-06-28 17:13:05 +0100

    :construction: feat: add common models to admin

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Tag, TaggedItem, Comment, Star, Technology
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ['name']}
+
+
+@admin.register(TaggedItem)
+class TaggedItemAdmin(admin.ModelAdmin):
+    list_display = ['tag', 'content_object', 'created_at']
+    readonly_fields = ['created_at']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'content_object', 'created_at']
+    search_fields = ['content']
+
+
+@admin.register(Star)
+class StarAdmin(admin.ModelAdmin):
+    list_display = ['user', 'content_object', 'starred_at']
+    readonly_fields = ['starred_at']
+
+
+@admin.register(Technology)
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'website']
+    search_fields = ['name']

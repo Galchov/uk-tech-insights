83fc1d6dae4815777096da7c91452be4cb3d5486
Zdravko <zdravko.galchov@gmail.com>
2025-07-01 19:14:48 +0100

    :hammer: refactor: add namespaces

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from . import views
 
 
-# app_name = 'accounts'
+app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name="login"),
--- apps/common/urls.py
+++ apps/common/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 
+app_name = 'common'
+
 urlpatterns = [
     path('', views.home_page_view, name="home"),
 ]

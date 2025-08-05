80625079998ccf9b043cc6e31f52a3c4b6a63d2c
Zdravko <zdravko.galchov@gmail.com>
2025-08-05 18:06:15 +0100

    :construction: feat(industries): complete app functionality

--- apps/industries/urls.py
+++ apps/industries/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
+from . import views
+
 
 app_name = 'industries'
 
+
 urlpatterns = [
-    
+    path('', views.IndustryListView.as_view(), name='industry_list'),
+    path('add/', views.IndustryCreateView.as_view(), name='industry_add'),
+    path('<int:pk>/edit/', views.IndustryUpdateView.as_view(), name='industry_edit'),
+    path('<int:pk>/delete/', views.IndustryDeleteView.as_view(), name='industry_delete'),
 ]
--- apps/industries/views.py
+++ apps/industries/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Industry
 
-# Create your views here.
+
+class IndustryAccessMixin(LoginRequiredMixin, UserPassesTestMixin):
+    def test_func(self):
+        user = self.request.user
+        return user.is_authenticated and (user.is_staff or user.groups.filter(name='Moderators').exists())
+
+
+class IndustryListView(IndustryAccessMixin, ListView):
+    model = Industry
+    template_name = 'industries/industry_list.html'
+    context_object_name = 'industries'
+
+
+class IndustryCreateView(IndustryAccessMixin, CreateView):
+    model = Industry
+    fields = ['name', 'description']
+    template_name = 'industries/industry_form.html'
+    success_url = reverse_lazy('industries:industry_list')
+
+
+class IndustryUpdateView(IndustryAccessMixin, UpdateView):
+    model = Industry
+    fields = ['name', 'description']
+    template_name = 'industries/industry_form.html'
+    success_url = reverse_lazy('industries:industry_list')
+
+
+class IndustryDeleteView(IndustryAccessMixin, DeleteView):
+    model = Industry
+    template_name = 'industries/industry_confirm_delete.html'
+    success_url = reverse_lazy('industries:industry_list')
